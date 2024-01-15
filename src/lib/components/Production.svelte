064077b4078846b0fc32e0429596c92face52c80
Kamil Patecki <paatorek@gmail.com>
2024-01-15 01:59:54 +0100

    Add JSSpeccy emulator

--- src/lib/components/Production.svelte
+++ src/lib/components/Production.svelte
@@ -3,6 +3,7 @@
 
 	export let prod: Prod;
 	let isHoveredOrActive = false;
+	let showEmu = false;
 </script>
 
 <div
@@ -24,30 +25,38 @@
 	</div>
 
 	{#if isHoveredOrActive}
-		<div class="absolute top-0 left-0 w-full">
+		<div class="absolute bottom-0 left-0 w-full">
 			<div class="flex justify-between items-center p-3 text-slate-200">
 				<div class="flex space-x-2">
 					{#if prod.download}
 						<a
 							href={prod.download}
-							class="p-1 font-mono text-sm border border-white rounded hover:bg-white hover:text-black transition-colors"
+							class="p-1 font-mono text-sm border border-slate-200 rounded hover:bg-slate-200 hover:text-black transition-colors"
 						>
 							Download
 						</a>
 					{/if}
+					{#if prod.download}
+						<button
+							on:click={() => (showEmu = true)}
+							class="p-1 font-mono text-sm border border-slate-200 rounded hover:bg-slate-200 hover:text-black transition-colors"
+						>
+							Demozoo
+						</button>
+					{/if}
 				</div>
 			</div>
 		</div>
-		<div class="absolute bottom-0 left-0 w-full p-4">
+		<div class="absolute top-0 left-0 w-full p-4">
 			<Typewriter delay={60} interval={40} keepCursorOnFinish={true}>
 				<h5 class="font-mono text-lg text-slate-200 mb-2">
 					{prod.name}
 				</h5>
 			</Typewriter>
 			<Typewriter delay={60} interval={40} keepCursorOnFinish={true}>
-				<h6 class="font-mono text-sm text-slate-400">
+				<h7 class="font-mono text-sm text-slate-400">
 					{prod.type}
-				</h6>
+				</h7>
 			</Typewriter>
 		</div>
 	{/if}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,21 @@
 <script lang="ts">
 	import Navbar from '$lib/components/Navbar.svelte';
+	import { onMount } from 'svelte';
 	import '../app.css';
+
+	let emu: any;
+	let isModalOpen = true;
+
+	onMount(() => {
+		if (window.JSSpeccy) {
+			emu = JSSpeccy(document.getElementById('jsspeccy'), { zoom: 3, machine: 128, sandbox: true });
+		}
+	});
 </script>
+
+<svelte:head>
+	<script src="/jsspeccy/jsspeccy.js"></script>
+</svelte:head>
 
 <div class="bg-slate-900">
 	<Navbar />
@@ -9,3 +23,30 @@
 		<slot />
 	</div>
 </div>
+
+{#if isModalOpen}
+	<div class="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+		<div class="relative bg-white p-1 rounded-lg shadow-lg">
+			<button
+				class="absolute top-0 right-0 mt-1 mr-1 text-black z-10"
+				on:click={() => (isModalOpen = false)}
+			>
+				<svg
+					class="h-6 w-6 z-60"
+					fill="none"
+					stroke="currentColor"
+					viewBox="0 0 24 24"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M6 18L18 6M6 6l12 12"
+					></path>
+				</svg>
+			</button>
+			<div id="jsspeccy"></div>
+		</div>
+	</div>
+{/if}
