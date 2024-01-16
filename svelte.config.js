367e9c7be527172b4a856f8bd11558f7742734ed
Kamil Patecki <paatorek@gmail.com>
2024-01-16 03:27:16 +0100

    Configure router, add favicon and title

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,8 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
@@ -11,11 +11,7 @@ const config = {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
+			fallback: '404.html'
 		})
 	}
 };
-
-export default config;
